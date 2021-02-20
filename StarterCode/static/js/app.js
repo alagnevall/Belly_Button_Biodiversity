bb1e97203fcbfc252c91f7e30bb0d4b09471265f
April <alagnevall@gmail.com>
2021-02-19 22:02:01 -0600

    added bubble chart and demographic info

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,7 +2,7 @@
 
 
 d3.json("samples.json").then(function(data){
-
+console.log(data)
     var selector = d3.select("#selDataset");
 
     data.names.forEach(function(choose, index){
@@ -10,6 +10,7 @@ d3.json("samples.json").then(function(data){
         option.text(choose);
         option.attr("value", index);
     })
+    optionChanged(0);
 });
 
 function optionChanged(result){
@@ -19,20 +20,22 @@ function optionChanged(result){
         // top10 = top10.slice(0,10);
 // console.log(top10);
 // console.log(subject.sample_values.slice(0,10));
-console.log(subject.otu_ids.toString());
+console.log(subject.otu_ids.slice(0,10));
 var trace1 = {
-    x: subject.sample_values.slice(0,10),
-    y: subject.otu_ids.toString(),
+    x: subject.sample_values.slice(0,10).reverse(),
+    y: subject.otu_ids.slice(0,10).reverse().map(d => "OTU " + d),
+    // y: ["a","b","c","d","e","f","g","h","i","j"],
     text: subject.otu_labels,
     name: "put a title",
     type: "bar",
-    orientation: "h"
+   orientation: "h"
 };
 var layout = {
-    title: "Greek gods search results",
+    title: "TOP 10 OTUs",
+    width: "auto",
     margin: {
       l: 100,
-      r: 100,
+      r: 0,
       t: 100,
       b: 100
     }
@@ -40,6 +43,54 @@ var layout = {
 var chartData = [trace1];
 Plotly.newPlot("bar", chartData, layout);
 
-    })
-
+var trace2 = {
+    y: subject.sample_values,
+    x: subject.otu_ids,
+    mode: "markers",
+    marker: {
+        size: subject.sample_values,
+        color: subject.otu_ids
+        // sizemode: "area"
+    },
+    text: subject.otu_labels
+        
 }
+var layout2 = {
+    title: "Bubbles",
+    margin: {
+      l: 100,
+      r: 0,
+      t: 100,
+      b: 100
+    }
+}
+var bubbleData = [trace2]
+Plotly.newPlot("bubble", bubbleData, layout2);
+
+var meta = data.metadata[result];
+console.log(Object.keys(meta))
+console.log(Object.values(meta))
+
+var subjectData = d3.select("#sample-metadata");
+
+
+// Object.entries(meta).forEach(function(key, value){
+//     subjectData.append("p")
+//     .text(key + ": " + value)
+
+subjectData.selectAll("p").remove()
+
+// })
+for (const [key, value] of Object.entries(meta)) {
+//   console.log(`${key}: ${value}`);
+  subjectData.append("p")
+    .text(key + ": " + value)
+};
+
+
+
+
+
+
+    });
+};
